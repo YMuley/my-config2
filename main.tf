50253f7641f71bb2b707a075ae06358e9e86211e
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-10-13 16:44:47 +0530

    new commit2

--- main.tf
+++ main.tf
@@ -1,61 +1,18 @@
-module "resource_Group" {
-  source  = "app.terraform.io/Motifworks/resource_Group/azurerm"
-  version = "1.0.2"
-    resource_group_list = [
-        # {
-        #     name        = "rg-ddi-poc" 
-        #     location    = "eastus"
-        #     tags        = {
-        #         location      = "eastus"
-        #         subscription  = "iac-dev"
-        #         environment   = "poc"
-        #     }
-        # },
-        # {
-        #     name        = "rg-ddi-dev" 
-        #     location    = "westus"
-        #     tags        = {
-        #         location      = "westus"
-        #         subscription  = "iac-dev"
-        #         environment   = "dev"
-        #     }
-        # }
-    ]
+data "terraform_remote_state" "primary-state" {
+  backend = "remote"
+
+  config = {
+    organization = "Motifworks"
+    workspaces = {
+      name = "primary-state"
+    }
+  }
+  
 }
 
-
-module "vnet" {
-  source  = "app.terraform.io/Motifworks/vnet/azurerm"
-  version = "1.0.0"
-  resource_group_output = module.resource_Group.resource_group_output
-  virtual_network_list = [
-    # {
-    #     name        = "vnet-ddi-poc"
-    #     location    = "eastus"
-    #     resource_group_name = "rg-ddi-poc"
-    #     address_space = ["10.100.0.0/16"] //["172.21.0.0/16"]
-    #     dns_server = [] //["172.21.1.40", "172.21.1.41"]
-    #     tags = {
-    #         environment = "poc"
-    #     }
-
-    # },
-    # {
-    #     name        = "vnet-ddi-dev"
-    #     location    = "westus"
-    #     resource_group_name = "rg-ddi-dev"
-    #     address_space = ["10.100.50.0/24"] //["172.21.0.0/16"]
-    #     dns_server = [] //["172.21.1.40", "172.21.1.41"]
-    #     tags = {
-    #         environment = "poc"
-    #     }
-
-    # }
-    ]
-
+output "remote_state" {
+  value = data.terraform_remote_state.primary-state
 }
-
-
 module "subnet" {
   source  = "app.terraform.io/Motifworks/subnet/azurerm"
   version = "1.0.0"
@@ -65,8 +22,8 @@ module "subnet" {
   vnet_subnet_list  =   [
     {
         name = "sub-ddi-poc-web"
-        resource_group_name     =   "rg-ddi-poc"
-        virtual_network_name    =   "vnet-ddi-poc"
+        resource_group_name     =   output.remote_state[rg-ddi-poc].name
+        virtual_network_name    =   output.remote_state[vnet-ddi-poc].name
         address_prefixes        =   ["10.100.0.0/24"]
         service_endpoints       =   ["Microsoft.Storage"]
         service_endpoint_policy_ids     =   ["/subscriptions/8694217e-4a30-4107-9a12-aeac74b82f5c/resourceGroups/rg-ddi-poc/providers/Microsoft.Network/serviceEndpointPolicies/ddi-test-poc/"]
